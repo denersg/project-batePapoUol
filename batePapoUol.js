172a4658a6aea62ab94a16c27391e32132da80ab
Dener Silva <dener.ufes@gmail.com>
2022-02-12 12:56:28 -0300

    Adicionada a parte de manter o usuario conectado na sala a cada 5 segundos

--- batePapoUol.js
+++ batePapoUol.js
@@ -1,28 +1,44 @@
+let userName;
+
 registerUserAndEnterTheRoom();
 
 function registerUserAndEnterTheRoom(){
-    const userName = prompt("Qual o seu nome?");
-    // console.log(userName)
+    userName = prompt("Qual o seu nome?");
+    
     const data = { name: userName };
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", data);
 
-    promise.then(quandoSucesso);
-    promise.catch(quandoErro);
+    promise.then(whenSuccessful);//Quando sucesso
+    promise.catch(inCaseOfError);//Quando erro
+
+    if(whenSuccessful){
+        stayConnected();
+    }
 }
 
-function quandoSucesso(response){
+//Verifica se requisição foi bem-sucedida
+function whenSuccessful(response){
     console.log("SUCESSO!!");
     console.log("Status code: " + response.status)
-    // console.log(response.data)
 }
 
-function quandoErro(error){
+//Verifica se requisição falhou
+function inCaseOfError(error){
     // Se a resposta for 400 é pq já tem um usuário com o nome
     // digitado. Então, eu devo chamar novamente a função de
     // registrar.
     if(error.response.status === 400){
+        alert("Este nome já está em uso. Por favor, digite outro nome.");
         registerUserAndEnterTheRoom();
     }
     console.log("Status code: " + error.response.status)
     console.log("Mensagem de erro: " + error.response.data)
+}
+
+//Mantém o usuário conectado
+function stayConnected(){
+    let data = {name: userName}, promise;
+    setInterval( function (){
+        promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", data);
+    }, 5000)
 }
